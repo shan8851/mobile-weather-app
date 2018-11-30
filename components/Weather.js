6e458a7779722d75dd5fc2ddf2faacf4eef702dc
shan5742 <asamshan456@gmail.com>
2018-11-30 07:58:07 +0000

    getting data dynamically from weather component

--- components/Weather.js
+++ components/Weather.js
@@ -2,15 +2,15 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const Weather = () => {
+const Weather = ({ weather, temperature }) => {
   return (
     <View style={styles.weatherContainer}>
       <View style={styles.headerContainer}>
         <MaterialCommunityIcons size={48} name="weather-sunny" color={"#fff"} />
-        <Text style={styles.tempText}>Temperature˚</Text>
+        <Text style={styles.tempText}>{temperature}˚</Text>
       </View>
       <View style={styles.bodyContainer}>
-        <Text style={styles.title}>So Sunny</Text>
+        <Text style={styles.title}>{weather}</Text>
         <Text style={styles.subtitle}>It hurts my eyes!</Text>
       </View>
     </View>
