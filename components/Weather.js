6b914ec68f5adfa44a3a926b9ca7239215c3fc1f
shan5742 <asamshan456@gmail.com>
2018-11-30 08:03:16 +0000

    adding different colours and text for various weather conditions

--- components/Weather.js
+++ components/Weather.js
@@ -1,34 +1,52 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
+import { weatherConditions } from "../utils/WeatherConditions";
 
 const Weather = ({ weather, temperature }) => {
   return (
-    <View style={styles.weatherContainer}>
+    <View
+      style={[
+        styles.weatherContainer,
+        { backgroundColor: weatherConditions[weather].color }
+      ]}
+    >
       <View style={styles.headerContainer}>
-        <MaterialCommunityIcons size={48} name="weather-sunny" color={"#fff"} />
+        <MaterialCommunityIcons
+          size={72}
+          name={weatherConditions[weather].icon}
+          color={"#fff"}
+        />
         <Text style={styles.tempText}>{temperature}˚</Text>
       </View>
       <View style={styles.bodyContainer}>
-        <Text style={styles.title}>{weather}</Text>
-        <Text style={styles.subtitle}>It hurts my eyes!</Text>
+        <Text style={styles.title}>{weatherConditions[weather].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherConditions[weather].subtitle}
+        </Text>
       </View>
     </View>
   );
 };
 
+Weather.propTypes = {
+  temperature: PropTypes.number.isRequired,
+  weather: PropTypes.string
+};
+
 const styles = StyleSheet.create({
   weatherContainer: {
-    flex: 1,
-    backgroundColor: "#f7b733"
+    flex: 1
   },
   headerContainer: {
     flex: 1,
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "space-around"
   },
   tempText: {
-    fontSize: 48,
+    fontSize: 72,
     color: "#fff"
   },
   bodyContainer: {
@@ -39,7 +57,7 @@ const styles = StyleSheet.create({
     marginBottom: 40
   },
   title: {
-    fontSize: 48,
+    fontSize: 60,
     color: "#fff"
   },
   subtitle: {
---
+++ utils/WeatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+  Rain: {
+    color: "#005BEA",
+    title: "Raining",
+    subtitle: "Don't forget your umbrella!",
+    icon: "weather-rainy"
+  },
+  Clear: {
+    color: "#f7b733",
+    title: "So Sunny",
+    subtitle: "It is hurting my eyes",
+    icon: "weather-sunny"
+  },
+  Thunderstorm: {
+    color: "#616161",
+    title: "A Storm is coming",
+    subtitle: "Be prepared!",
+    icon: "weather-lightning"
+  },
+  Clouds: {
+    color: "#1F1C2C",
+    title: "Clouds",
+    subtitle: "Everywhere",
+    icon: "weather-cloudy"
+  },
+
+  Snow: {
+    color: "#00d2ff",
+    title: "Snow",
+    subtitle: "Get out and build a snowman!",
+    icon: "weather-snowy"
+  },
+  Drizzle: {
+    color: "#076585",
+    title: "Drizzle",
+    subtitle: "That fine rain...",
+    icon: "weather-hail"
+  },
+  Haze: {
+    color: "#66A6FF",
+    title: "Haze",
+    subtitle: "Another name for Partial Raining",
+    icon: "weather-hail"
+  },
+  Mist: {
+    color: "#3CD3AD",
+    title: "Mist",
+    subtitle: "Creepy!",
+    icon: "weather-fog"
+  }
+};
